a346a8627818d9f1c1c633548105dac52429d388
user <user@localhost.localdomain>
2025-12-17 00:58:26 +0300

    corrected order placement

---
+++ build/CMakeFiles/freelance_exchange.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/JsonClass.cpp" "CMakeFiles/freelance_exchange.dir/code/src/JsonClass.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/JsonClass.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/admin.cpp" "CMakeFiles/freelance_exchange.dir/code/src/admin.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/admin.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/cli/SignIn.cpp" "CMakeFiles/freelance_exchange.dir/code/src/cli/SignIn.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/cli/SignIn.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/cli/SignUp.cpp" "CMakeFiles/freelance_exchange.dir/code/src/cli/SignUp.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/cli/SignUp.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/cli/admincli.cpp" "CMakeFiles/freelance_exchange.dir/code/src/cli/admincli.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/cli/admincli.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/cli/customercli.cpp" "CMakeFiles/freelance_exchange.dir/code/src/cli/customercli.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/cli/customercli.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/cli/performercli.cpp" "CMakeFiles/freelance_exchange.dir/code/src/cli/performercli.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/cli/performercli.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/customer.cpp" "CMakeFiles/freelance_exchange.dir/code/src/customer.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/customer.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/database.cpp" "CMakeFiles/freelance_exchange.dir/code/src/database.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/database.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/main.cpp" "CMakeFiles/freelance_exchange.dir/code/src/main.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/main.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/order.cpp" "CMakeFiles/freelance_exchange.dir/code/src/order.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/order.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/password.cpp" "CMakeFiles/freelance_exchange.dir/code/src/password.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/password.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/performer.cpp" "CMakeFiles/freelance_exchange.dir/code/src/performer.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/performer.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/review.cpp" "CMakeFiles/freelance_exchange.dir/code/src/review.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/review.cpp.o.d"
+  "/home/user/freelance_exchange_1/freelance_exchange/code/src/user.cpp" "CMakeFiles/freelance_exchange.dir/code/src/user.cpp.o" "gcc" "CMakeFiles/freelance_exchange.dir/code/src/user.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
